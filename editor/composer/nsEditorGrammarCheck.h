39902b48cf18cc68e0cc30ac57891ec068514e9a
lenny93 <lcutugno@msn.com>
2015-08-13 01:42:24 -0600

    fixing compiler errors

--- editor/composer/nsEditorGrammarCheck.h
+++ editor/composer/nsEditorGrammarCheck.h
@@ -1,76 +1,46 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef __SPECIALTHING_IMPL_H__
+#define __SPECIALTHING_IMPL_H__
 
-#ifndef nsEditorSpellCheck_h___
-#define nsEditorSpellCheck_h___
+#include "nsIEditorGrammarCheck.h"
+#include "mozilla/Attributes.h"
+#include "nsString.h"
 
+#define NS_GRAMMARCHECK_CONTRACTID "@mozilla.org/grammarcheck;1"
+#define NS_GRAMMARCHECK_CLASSNAME "EditorGrammarCheck"
+#define NS_GRAMMARCHECK_CID { 0x8ac26150, 0x586f, 0x4b70, \
+  { 0x90, 0xdf, 0x07, 0x61, 0x0b, 0x80, 0xb4, 0x5f } }
 
-#include "nsCOMPtr.h"                   // for nsCOMPtr
-#include "nsCycleCollectionParticipant.h"
-#include "nsIEditorSpellCheck.h"        // for NS_DECL_NSIEDITORSPELLCHECK, etc
-#include "nsISupportsImpl.h"
-#include "nsString.h"                   // for nsString
-#include "nsTArray.h"                   // for nsTArray
-#include "nscore.h"                     // for nsresult
-
-class nsIEditor;
-class nsISpellChecker;
-class nsITextServicesFilter;
-
-#define NS_EDITORSPELLCHECK_CID                     \
-{ /* {75656ad9-bd13-4c5d-939a-ec6351eea0cc} */        \
-    0x75656ad9, 0xbd13, 0x4c5d,                       \
-    { 0x93, 0x9a, 0xec, 0x63, 0x51, 0xee, 0xa0, 0xcc }\
-}
-
-class DictionaryFetcher;
-
-class nsEditorSpellCheck final : public nsIEditorSpellCheck
+class nsEditorGrammarCheck : public nsIEditorGrammarCheck
 {
-  friend class DictionaryFetcher;
-
 public:
-  nsEditorSpellCheck();
+	nsEditorGrammarCheck();
 
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-  NS_DECL_CYCLE_COLLECTION_CLASS(nsEditorSpellCheck)
+    /**
+     * This macro expands into a declaration of the nsISupports interface.
+     * Every XPCOM component needs to implement nsISupports, as it acts
+     * as the gateway to other interfaces this component implements.  You
+     * could manually declare QueryInterface, AddRef, and Release instead
+     * of using this macro, but why?
+     */
+    // nsISupports interface
+    NS_DECL_ISUPPORTS
 
-  /* Declare all methods in the nsIEditorSpellCheck interface */
-  NS_DECL_NSIEDITORSPELLCHECK
+    /**
+     * This macro is defined in the nsISample.h file, and is generated
+     * automatically by the xpidl compiler.  It expands to
+     * declarations of all of the methods required to implement the
+     * interface.  xpidl will generate a NS_DECL_[INTERFACENAME] macro
+     * for each interface that it processes.
+     *
+     * The methods of nsISample are discussed individually below, but
+     * commented out (because this macro already defines them.)
+     */
+    NS_DECL_NSIEDITORGRAMMARCHECK
 
-protected:
-  virtual ~nsEditorSpellCheck();
+private:
+	~nsEditorGrammarCheck();
 
-  nsCOMPtr<nsISpellChecker> mSpellChecker;
-
-  nsTArray<nsString>  mSuggestedWordList;
-  int32_t        mSuggestedWordIndex;
-
-  // these are the words in the current personal dictionary,
-  // GetPersonalDictionary must be called to load them.
-  nsTArray<nsString>  mDictionaryList;
-  int32_t        mDictionaryIndex;
-
-  nsresult       DeleteSuggestedWordList();
-
-  nsCOMPtr<nsITextServicesFilter> mTxtSrvFilter;
-  nsCOMPtr<nsIEditor> mEditor;
-
-  nsString mPreferredLang;
-
-  uint32_t mDictionaryFetcherGroup;
-
-  bool mUpdateDictionaryRunning;
-
-  nsresult DictionaryFetched(DictionaryFetcher* aFetchState);
-
-public:
-  void BeginUpdateDictionary() { mUpdateDictionaryRunning = true ;}
-  void EndUpdateDictionary() { mUpdateDictionaryRunning = false ;}
+    char* mValue;
 };
 
-#endif // nsEditorSpellCheck_h___
-
-
+#endif
