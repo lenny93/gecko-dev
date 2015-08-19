fea7fb9d3b01c80e5c5810477aaa57d10b90091a
lenny93 <lcutugno@msn.com>
2015-08-18 19:31:15 -0600

    m2

--- editor/composer/nsEditorGrammarCheck.cpp
+++ editor/composer/nsEditorGrammarCheck.cpp
@@ -1,121 +1,92 @@
-#include <stdio.h>
 #include "nsEditorGrammarCheck.h"
-
 #include "nsMemory.h"
-#include "nsEmbedString.h"
 #include "nsIClassInfoImpl.h"
+#include "mozilla/ModuleUtils.h"
+#include <string.h>                     // for nullptr, strcmp
+#include <windows.h>
 
-NS_IMPL_ISUPPORTS1(nsEditorGrammarCheck, nsIEditorGrammarCheck)
 
-nsEditorGrammarCheck::nsEditorGrammarCheck() : mValue(nullptr)
+using namespace mozilla;
+
+GC_FACTORY_SINGLETON_IMPLEMENTATION(nsEditorGrammarCheck, gGrammarCheckService)
+NS_IMPL_CLASSINFO(nsEditorGrammarCheck, nullptr, 0, NS_GRAMMARCHECK_CID)
+NS_IMPL_ISUPPORTS_CI(nsEditorGrammarCheck, nsIEditorGrammarCheck)
+
+
+nsEditorGrammarCheck::nsEditorGrammarCheck() 
 {
-    mValue = (char*)nsMemory::Clone("initial value", 14);
+	NS_ASSERTION(!gGrammarCheckService,
+			   "Attempting to create two instances of the service!");
+	gGrammarCheckService = this;
 }
 
-nsEditorGrammarCheck::~nsEditorGrammarCheck()
+nsEditorGrammarCheck::~nsEditorGrammarCheck() 
 {
-    if (mValue)
-        nsMemory::Free(mValue);
+	NS_ASSERTION(gGrammarCheckService == this,
+			   "Deleting a non-singleton instance of the service");
+	if (gGrammarCheckService == this)
+		gGrammarCheckService = nullptr;
 }
 
-
-NS_IMPL_CLASSINFO(nsEditorGrammarCheck, NULL, 0, NS_GRAMMARCHECK_CID)
-NS_IMPL_ISUPPORTS1_CI(nsEditorGrammarCheck, nsIEditorGrammarCheck)
-
-/**
- * Notice that in the protoype for this function, the NS_IMETHOD macro was
- * used to declare the return type.  For the implementation, the return
- * type is declared by NS_IMETHODIMP
- */
-NS_IMETHODIMP nsEditorGrammarCheck::GetValue(char** aValue)
+nsresult nsEditorGrammarCheck::Init()
 {
-    NS_PRECONDITION(aValue != nullptr, "null ptr");
-    if (! aValue)
-        return NS_ERROR_NULL_POINTER;
 
-    if (mValue) {
-        /**
-         * GetValue's job is to return data known by an instance of
-         * nsSampleImpl to the outside world.  If we  were to simply return 
-         * a pointer to data owned by this instance, and the client were to
-         * free it, bad things would surely follow.
-         * On the other hand, if we create a new copy of the data for our
-         * client, and it turns out that client is implemented in JavaScript,
-         * there would be no way to free the buffer.  The solution to the 
-         * buffer ownership problem is the nsMemory singleton.  Any buffer
-         * returned by an XPCOM method should be allocated by the nsMemory.
-         * This convention lets things like JavaScript reflection do their
-         * job, and simplifies the way C++ clients deal with returned buffers.
-         */
-        *aValue = (char*) nsMemory::Clone(mValue, strlen(mValue) + 1);
-        if (! *aValue)
-            return NS_ERROR_NULL_POINTER;
-    }
-    else {
-        *aValue = nullptr;
-    }
-    return NS_OK;
-}
-
-NS_IMETHODIMP nsEditorGrammarCheck::SetValue(const char* aValue)
-{
-    NS_PRECONDITION(aValue != nullptr, "null ptr");
-    if (! aValue)
-        return NS_ERROR_NULL_POINTER;
-
-    if (mValue) {
-        nsMemory::Free(mValue);
-    }
-
-    /**
-     * Another buffer passing convention is that buffers passed INTO your
-     * object ARE NOT YOURS.  Keep your hands off them, unless they are
-     * declared "inout".  If you want to keep the value for posterity,
-     * you will have to make a copy of it.
-     */
-    mValue = (char*) nsMemory::Clone(aValue, strlen(aValue) + 1);
-    return NS_OK;
+  return NS_OK;
 }
 
 NS_IMETHODIMP nsEditorGrammarCheck::Poke(const char* aValue)
 {
-    return SetValue((char*) aValue);
-}
-
-
-static void nsEditorGrammarCheck(nsACString& aValue)
-{
-    NS_CStringSetData(aValue, "GetValue");
-}
-
-NS_IMETHODIMP nsEditorGrammarCheck::WriteValue(const char* aPrefix)
-{
-    NS_PRECONDITION(aPrefix != nullptr, "null ptr");
-    if (! aPrefix)
-        return NS_ERROR_NULL_POINTER;
-
-    printf("%s %s\n", aPrefix, mValue);
-
-    // This next part illustrates the nsEmbedString:
-    nsEmbedString foopy;
-    foopy.Append(PRUnichar('f'));
-    foopy.Append(PRUnichar('o'));
-    foopy.Append(PRUnichar('o'));
-    foopy.Append(PRUnichar('p'));
-    foopy.Append(PRUnichar('y'));
-    
-    const PRUnichar* f = foopy.get();
-    uint32_t l = foopy.Length();
-    printf("%c%c%c%c%c %d\n", char(f[0]), char(f[1]), char(f[2]), char(f[3]), char(f[4]), l);
-    
-    nsEmbedCString foopy2;
-    GetStringValue(foopy2);
-
-    //foopy2.AppendLiteral("foopy");
-    const char* f2 = foopy2.get();
-    uint32_t l2 = foopy2.Length();
-
-    printf("%s %d\n", f2, l2);
-
+	MessageBox(0, "Hello", "Meow", 2);
     return NS_OK;
 }
+
+
+NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(nsEditorGrammarCheck, nsEditorGrammarCheck::GetSingleton)
+
+// The following line defines a kNS_SAMPLE_CID CID variable.
+NS_DEFINE_NAMED_CID(NS_GRAMMARCHECK_CID);
+
+// Build a table of ClassIDs (CIDs) which are implemented by this module. CIDs
+// should be completely unique UUIDs.
+// each entry has the form { CID, service, factoryproc, constructorproc }
+// where factoryproc is usually NULL.
+static const mozilla::Module::CIDEntry kSampleCIDs[] = {
+    { &kNS_GRAMMARCHECK_CID, false, nullptr, nsEditorGrammarCheckConstructor },
+    { nullptr }
+};
+
+// Build a table which maps contract IDs to CIDs.
+// A contract is a string which identifies a particular set of functionality. In some
+// cases an extension component may override the contract ID of a builtin gecko component
+// to modify or extend functionality.
+static const mozilla::Module::ContractIDEntry kSampleContracts[] = {
+    { NS_GRAMMARCHECK_CONTRACTID, &kNS_GRAMMARCHECK_CID },
+    { nullptr }
+};
+
+// Category entries are category/key/value triples which can be used
+// to register contract ID as content handlers or to observe certain
+// notifications. Most modules do not need to register any category
+// entries: this is just a sample of how you'd do it.
+// @see nsICategoryManager for information on retrieving category data.
+static const mozilla::Module::CategoryEntry kSampleCategories[] = {
+    { "my-category", "my-key", NS_GRAMMARCHECK_CONTRACTID },
+    { nullptr }
+};
+
+static const mozilla::Module kSampleModule = {
+    mozilla::Module::kVersion,
+    kSampleCIDs,
+    kSampleContracts,
+    kSampleCategories
+};
+
+// The following line implements the one-and-only "NSModule" symbol exported from this
+// shared library.
+NSMODULE_DEFN(nsGrammarCheckModule) = &kSampleModule;
+
+// The following line implements the one-and-only "NSGetModule" symbol
+// for compatibility with mozilla 1.9.2. You should only use this
+// if you need a binary which is backwards-compatible and if you use
+// interfaces carefully across multiple versions.
+//NS_IMPL_MOZILLA192_NSGETMODULE(&kSampleModule)
--- editor/composer/nsEditorGrammarCheck.h
+++ editor/composer/nsEditorGrammarCheck.h
@@ -2,18 +2,52 @@
 #define __SPECIALTHING_IMPL_H__
 
 #include "nsIEditorGrammarCheck.h"
-#include "mozilla/Attributes.h"
-#include "nsString.h"
 
 #define NS_GRAMMARCHECK_CONTRACTID "@mozilla.org/grammarcheck;1"
-#define NS_GRAMMARCHECK_CLASSNAME "EditorGrammarCheck"
+#define NS_GRAMMARCHECK_CLASSNAME "nsEditorGrammarCheck"
 #define NS_GRAMMARCHECK_CID { 0x8ac26150, 0x586f, 0x4b70, \
   { 0x90, 0xdf, 0x07, 0x61, 0x0b, 0x80, 0xb4, 0x5f } }
+  
+#define GC_FACTORY_SINGLETON_IMPLEMENTATION(_className, _sInstance)        \
+_className * _className::_sInstance = nullptr;                                \
+																		   \
+already_AddRefed<_className>                                                 \
+_className::GetSingleton()                                                   \
+{                                                                            \
+if (_sInstance) {                                                          \
+  nsRefPtr<_className> ret = _sInstance;                                   \
+  return ret.forget();                                                     \
+}                                                                          \
+_sInstance = new _className();                                             \
+nsRefPtr<_className> ret = _sInstance;                                     \
+if (NS_FAILED(_sInstance->Init())) {                                       \
+  /* Null out ret before _sInstance so the destructor doesn't assert */    \
+  ret = nullptr;                                                           \
+  _sInstance = nullptr;                                                    \
+  return nullptr;                                                          \
+}                                                                          \
+return ret.forget();                                                       \
+}
 
-class nsEditorGrammarCheck : public nsIEditorGrammarCheck
+class nsEditorGrammarCheck final : public nsIEditorGrammarCheck
 {
 public:
 	nsEditorGrammarCheck();
+	
+	static already_AddRefed<nsEditorGrammarCheck> GetSingleton();
+	
+	static nsEditorGrammarCheck* GetGrammarCheckService()
+	{
+		if (!gGrammarCheckService) 
+		{
+			nsCOMPtr<nsIEditorGrammarCheck> serv = do_GetService(NS_GRAMMARCHECK_CONTRACTID);
+			NS_ENSURE_TRUE(serv, nullptr);
+			NS_ASSERTION(gGrammarCheckService, "Should have static instance pointer now");
+		}
+		return gGrammarCheckService;
+	}
+	
+	nsresult Init();
 
     /**
      * This macro expands into a declaration of the nsISupports interface.
@@ -36,11 +70,10 @@ public:
      * commented out (because this macro already defines them.)
      */
     NS_DECL_NSIEDITORGRAMMARCHECK
-
+	
 private:
 	~nsEditorGrammarCheck();
-
-    char* mValue;
+	static nsEditorGrammarCheck* gGrammarCheckService;
 };
 
 #endif
